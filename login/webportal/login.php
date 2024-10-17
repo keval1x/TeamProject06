7b8ee76fe3425bcf97a5c1b166b6db7790ba012e
Edgar Fleming <mail@c4t.uk>
2024-10-17 15:38:51 +0100

    Add files via upload

---
+++ login/webportal/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Webportal Login</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body onload="" style="background-color: #FFF;">
+    <div id="login-style"></div>
+    <header></header>
+    <b>
+    </b>
+    <script src="js/lreqs.js"></script>
+</body>
+</html>
